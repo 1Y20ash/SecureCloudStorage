23af4f434ec4a41d34f889d1927567fb811db488
1Y20ash <yashchitmalwar@gmail.com>
2026-08-17 16:38:09 +0530

    Add AES-256-GCM decryption support

--- crypto/encryption.py
+++ crypto/encryption.py
@@ -1,5 +1,6 @@
 import os
 
+from cryptography.exceptions import InvalidTag
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
@@ -8,6 +9,7 @@ SALT_SIZE = 16
 NONCE_SIZE = 12
 KEY_SIZE = 32
 PBKDF2_ITERATIONS = 600_000
+MAGIC = b"SCS1"
 
 
 def derive_key(password: str, salt: bytes) -> bytes:
@@ -24,11 +26,34 @@ def derive_key(password: str, salt: bytes) -> bytes:
 def encrypt_file(file_bytes: bytes, password: str) -> bytes:
     """Encrypt bytes using AES-256-GCM.
 
-    The returned format is:
-        magic | salt | nonce | ciphertext+authentication-tag
+    Stored format:
+        magic | salt | nonce | ciphertext + authentication tag
     """
     salt = os.urandom(SALT_SIZE)
     nonce = os.urandom(NONCE_SIZE)
     key = derive_key(password, salt)
     ciphertext = AESGCM(key).encrypt(nonce, file_bytes, None)
-    return b"SCS1" + salt + nonce + ciphertext
+    return MAGIC + salt + nonce + ciphertext
+
+
+def decrypt_file(encrypted_data: bytes, password: str) -> bytes:
+    """Decrypt data produced by encrypt_file.
+
+    Invalid passwords or modified ciphertext raise InvalidTag.
+    """
+    minimum_size = len(MAGIC) + SALT_SIZE + NONCE_SIZE + 16
+    if len(encrypted_data) < minimum_size:
+        raise ValueError("Invalid encrypted file.")
+
+    if encrypted_data[: len(MAGIC)] != MAGIC:
+        raise ValueError("Invalid encrypted file format.")
+
+    offset = len(MAGIC)
+    salt = encrypted_data[offset : offset + SALT_SIZE]
+    offset += SALT_SIZE
+    nonce = encrypted_data[offset : offset + NONCE_SIZE]
+    offset += NONCE_SIZE
+    ciphertext = encrypted_data[offset:]
+
+    key = derive_key(password, salt)
+    return AESGCM(key).decrypt(nonce, ciphertext, None)
