a2e9416bc0d79c39f1e84d5297725666ff79d33b
1Y20ash <yashchitmalwar@gmail.com>
2026-08-26 19:33:19 +0530

    feat: register Phase 4 routes during app initialization

--- extensions.py
+++ extensions.py
@@ -3,6 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 db = SQLAlchemy()
-login_manager = LoginManager()
+
+
+class ApplicationLoginManager(LoginManager):
+    """Login manager that registers application extensions after app creation."""
+
+    def init_app(self, app, add_context_processor=True):
+        super().init_app(app, add_context_processor=add_context_processor)
+        from phase4_routes import register_phase4_routes
+        register_phase4_routes(app)
+
+
+login_manager = ApplicationLoginManager()
 login_manager.login_view = "login"
 login_manager.login_message = "Please log in to access this page."
